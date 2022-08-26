b945166bd3ddf8369e7e20e767f27cd6a1026dd0
Chris Ecker <ecker.chris.r@gmail.com>
2022-08-26 10:26:10 -0500

    moving html contents into app

--- slacker525600/src/App.tsx
+++ slacker525600/src/App.tsx
@@ -6,19 +6,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Code <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="title">
+            The lazy dev
+        </div>
+        <img width="100%" src="https://lh3.googleusercontent.com/pw/AL9nZEUT7_j57bRvdnEJrT0baJ7HE53AMwjwnwYGjvooPid5D2XXtCpSmUXBdG7CvyKKEaG3GZBbjWyjnsJROYgIO2OvHdg6BqnTk_4ATc7ve802_m0YrVLB5ph52A97OjLlnwOsmte1kanRhokrt7qTQuq4jg=w1680-h1260-no?authuser=0" />
+
+    Hello World!
+     thoughts on things I'll likely put into this page 
+        links to other things I've done,
+        styling the page to not look like a blank white nothing
+        what am I trying to convey with personal page why am I putting this out there at all 
+        blog personal feelings about whatever ... likely a link to another provider vs actually hosted here
+        how to contact me 
+        personal hobbies and projects 
+        lulz
       </header>
+     <div className="footer">
+        copyright chris ecker 2022
+        
+        <a href="www.twitter.com/slacker525600"><img alt="bird" src="" /></a>   
+        <a href="www.facebook.com/slacker525600"> <img alt="face" src="" />  </a>
+        <a href="www.github.com/slacker525600" ><img alt="git" src="" /> </a>
+        <a href="www.linkedin.com/slacker525600"> <img alt="work" src="l" /> </a>
+     </div>
+
     </div>
   );
 }
