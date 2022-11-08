fb153e9794bc45320ae8f1a5b18a3af0c345895c
Christopher Ecker <christopherecker@Christophers-MacBook-Pro.local>
2022-11-08 16:05:08 -0600

    Adding some images, a tiny bit of styling

---
+++ docs/precache-manifest.f4f0f7c27c1f32cd876acb824d4f1b3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e15b5524d19bfdf063e",
+    "url": "/static/css/main.14864d38.chunk.css"
+  },
+  {
+    "revision": "1e15b5524d19bfdf063e",
+    "url": "/static/js/main.1e15b552.chunk.js"
+  },
+  {
+    "revision": "bfa36157bcc75fb8ea07",
+    "url": "/static/js/1.bfa36157.chunk.js"
+  },
+  {
+    "revision": "7587d2c9267632307ee0",
+    "url": "/static/js/2.7587d2c9.chunk.js"
+  },
+  {
+    "revision": "0da2dd41623d9513095b",
+    "url": "/static/js/runtime~main.0da2dd41.js"
+  },
+  {
+    "revision": "cb355f0e1c6b675a7722f6fb120f4360",
+    "url": "/index.html"
+  }
+];
--- slacker525600/src/App.tsx
+++ slacker525600/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -9,25 +8,48 @@ function App() {
         <div className="title">
             The lazy dev
         </div>
-        <img width="100%" src="https://lh3.googleusercontent.com/pw/AL9nZEUT7_j57bRvdnEJrT0baJ7HE53AMwjwnwYGjvooPid5D2XXtCpSmUXBdG7CvyKKEaG3GZBbjWyjnsJROYgIO2OvHdg6BqnTk_4ATc7ve802_m0YrVLB5ph52A97OjLlnwOsmte1kanRhokrt7qTQuq4jg=w1680-h1260-no?authuser=0" />
-
-    Hello World!
-     thoughts on things I'll likely put into this page 
-        links to other things I've done,
-        styling the page to not look like a blank white nothing
-        what am I trying to convey with personal page why am I putting this out there at all 
-        blog personal feelings about whatever ... likely a link to another provider vs actually hosted here
-        how to contact me 
-        personal hobbies and projects 
-        lulz
-      </header>
-     <div className="footer">
-        copyright chris ecker 2022
         
-        <a href="www.twitter.com/slacker525600"><img alt="bird" src="" /></a>   
-        <a href="www.facebook.com/slacker525600"> <img alt="face" src="" />  </a>
-        <a href="www.github.com/slacker525600" ><img alt="git" src="" /> </a>
-        <a href="www.linkedin.com/slacker525600"> <img alt="work" src="l" /> </a>
+      </header>
+      <body>
+          <div className='image'>
+            <img src="https://lh3.googleusercontent.com/pw/AL9nZEUT7_j57bRvdnEJrT0baJ7HE53AMwjwnwYGjvooPid5D2XXtCpSmUXBdG7CvyKKEaG3GZBbjWyjnsJROYgIO2OvHdg6BqnTk_4ATc7ve802_m0YrVLB5ph52A97OjLlnwOsmte1kanRhokrt7qTQuq4jg=w1680-h1260-no?authuser=0" />
+          </div>
+          
+          <div className='messagebox'>
+            <div className='message'>
+              Hello World!
+              <br />
+              thoughts on things I'll likely put into this page 
+              links to other things I've done,
+              styling the page to not look like a blank white nothing
+              what am I trying to convey with personal page why am I putting this out there at all 
+              blog personal feelings about whatever ... likely a link to another provider vs actually hosted here
+              how to contact me 
+              personal hobbies and projects 
+              lulz
+
+            </div>
+          </div>
+
+      </body>
+
+
+     <div className="footer">
+        
+        <a href="https://www.twitter.com/slacker525600">
+          <img height={32} width={32} alt="bird" src={require('./small_goose.png')} /> 
+        </a>
+        <a href="https://www.facebook.com/slacker525600"> 
+          <img height={32} width={32} alt="face" src={require('./face.png')} />  
+        </a>
+        <a href="https://www.github.com/slacker525600" > 
+          <img height={32} width={32} alt="git" src={require('./code.png')} />
+        </a>
+        <a href="https://www.linkedin.com/in/chrisecker">
+          <img height={32} width={32} alt="work" src={require('./work.png')} /> 
+        </a>
+        <br />
+        copyright chris ecker 2022
      </div>
 
     </div>
