d604cfdcd5aae5b9e086f570872d2beb21f1c23b
Christopher Ecker <christopherecker@Christophers-MacBook-Pro.local>
2022-11-08 23:28:11 -0600

    Think this is working and going to sleep

--- slacker525600/src/footerLinks.tsx
+++ slacker525600/src/footerLinks.tsx
@@ -1,5 +1,4 @@
-import { FC } from "react"
-import { ArrayBindingElement } from "typescript";
+import React, {FC } from 'react';
 
 /*
 goal of making a reusable component for footer links, 
@@ -8,7 +7,10 @@ will also include sub component to render individual logo/link
 interface IProps {
 }
 
-interface FooterProp {
+interface FooterLinksProp {
+    items: Array<FooterProp>
+};
+export interface FooterProp {
     // a footer (in this context) consists of a list of links with thumbnails and alt text 
     link: string;
     image: string;
@@ -23,21 +25,23 @@ https://www.twitter.com/slacker525600,small_goose.png,bird
 https://www.facebook.com/slacker525600,face.png,face
 https://www.github.com/slacker525600,code.png,code
 https://www.linkedin.com/in/chrisecker,work.png,work
-
-//todo standardize on alttxt=filename
-
-footerList = [{}]
 */
 
 const FooterLink:FC<FooterProp> = 
 ({ link, altText, image}) => 
-    <a href={link}>
-        <img height={50} width={50} alt={altText} src={image} /> 
+    <a key={link} href={link}>
+        <img height={50} width={50} alt={altText} src={require('./' +image)} /> 
     </a>;
 
-const FooterLinks:FC<Array<FooterProp>> = (items) => 
-    <div> 
-        { items.map((item) => <FooterLink link={item.link} altText={item.altText} image={item.image} />) }
-    </div>;
+const FooterLinks:FC<FooterLinksProp> = ({items}) => {
+    console.log(items)
+    return (
+        <div className="footer">
+            { items.map((item) => <FooterLink link={item.link} altText={item.altText} image={item.image} />) }
+            <br />
+            copyright chris ecker 2022
+        </div>
+)};
+
 
 export default FooterLinks;
