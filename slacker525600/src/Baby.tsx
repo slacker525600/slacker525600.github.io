1365b67b08c2f50d393e4c74dc9f1f10b2c5ae20
Chris E <ecker.chris.r@gmail.com>
2022-11-10 14:19:21 -0600

    Babyidea (#2)
    * adding new index for baby hidden app ... non functional
    
    * Added ifpa link to footer list, moved list to footer file, added react router and baby component tied in, added types for react-router, and an error page for the same, added README to top level folder just to have something there
    
    Co-authored-by: Christopher Ecker <christopherecker@Christophers-MacBook-Pro.local>
    Co-authored-by: slacker525600 <slacker525600@yahoo.com>

---
+++ slacker525600/src/Baby.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+
+/*
+Current idea is to provide an easy to use baby size comparison application.
+lets start with on/off,
+then move to days since fertilization yielding mass
+then move to all the other things (types/weightvslength/)
+then worry about UX of interaction of above ... ratios lulz
+
+lets do this with hooks ...
+, useEffect, useState
+
+export interface BabyProp {
+  // noted above path forward, starting with on/off bool and number
+  days: number
+};
+*/
+
+const Baby: FC = () => {
+  return (
+    <div>Hello World</div>
+  )
+}
+
+export default Baby
--- slacker525600/src/footerLinks.tsx
+++ slacker525600/src/footerLinks.tsx
@@ -3,39 +3,42 @@ import React, { FC } from 'react'
 /*
 goal of making a reusable component for footer links,
 will also include sub component to render individual logo/link
+csv format of inputs
+link,image,altText
+https://www.twitter.com/slacker525600,small_goose.png,bird
+https://www.facebook.com/slacker525600,face.png,face
+https://www.github.com/slacker525600,code.png,code
+https://www.linkedin.com/in/chrisecker,work.png,work
+https://www.ifpapinball.com/player.php?p=69771,ifpa.png,ifpa
 */
-interface FooterLinksProp {
-  items: FooterProp[]
-};
-export interface FooterProp {
+
+interface FooterProp {
   // a footer (in this context) consists of a list of links with thumbnails and alt text
   link: string
   image: string
   altText: string
 };
 
-/*
-csv format of inputs
-
-link, image, altText
-https://www.twitter.com/slacker525600,small_goose.png,bird
-https://www.facebook.com/slacker525600,face.png,face
-https://www.github.com/slacker525600,code.png,code
-https://www.linkedin.com/in/chrisecker,work.png,work
-*/
+const footerVals: FooterProp[] = [
+  { link: 'https://www.twitter.com/slacker525600', image: 'small_goose.png', altText: 'bird' },
+  { link: 'https://www.facebook.com/slacker525600', image: 'face.png', altText: 'face' },
+  { link: 'https://www.github.com/slacker525600', image: 'code.png', altText: 'code' },
+  { link: 'https://www.linkedin.com/in/chrisecker', image: 'work.png', altText: 'work' },
+  { link: 'https://www.ifpapinball.com/player.php?p=69771', image: 'ifpa.png', altText: 'ifpa' }
+]
 
 const FooterLink: FC<FooterProp> =
 ({ link, altText, image }) =>
-    <a key={link} href={link}>
-        <img height={50} width={50} alt={altText} src={require('./' + image)} />
-    </a>
+  <a key={link} href={link}>
+      <img height={50} width={50} alt={altText} src={require('./' + image)} />
+  </a>
 
-const FooterLinks: FC<FooterLinksProp> = ({ items }) => (
-        <div className="footer">
-            { items.map((item) => <FooterLink key={item.link} link={item.link} altText={item.altText} image={item.image} />) }
-            <br />
-            copyright chris ecker 2022
-        </div>
+const FooterLinks: FC = () => (
+  <div className="footer">
+      { footerVals.map((item) => <FooterLink key={item.link} link={item.link} altText={item.altText} image={item.image} />) }
+      <br />
+      copyright chris ecker 2022
+  </div>
 )
 
 export default FooterLinks
