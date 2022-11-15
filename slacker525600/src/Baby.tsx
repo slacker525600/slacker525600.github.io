a08728664de4feb7796e524ad2f1e76123f6a896
slacker525600 <slacker525600@yahoo.com>
2022-11-15 13:40:49 -0600

    Hooking up a little state management for components

--- slacker525600/src/Baby.tsx
+++ slacker525600/src/Baby.tsx
@@ -1,46 +1,66 @@
-import React, { FC } from 'react'
+import React, { ChangeEvent, FC, useState } from 'react'
 import FooterLinks from './footerLinks'
 
 /*
 Current idea is to provide an easy to use baby size comparison application.
-lets start with on/off,
 then move to days since fertilization yielding mass
 then move to all the other things (types/weightvslength/)
 then worry about UX of interaction of above ... ratios lulz
-
-lets do this with hooks ...
-, useEffect, useState
-
-export interface BabyProp {
-  // noted above path forward, starting with on/off bool and number
-  days: number
-};
 */
 
-const BabyOutputs: FC = () => {
+// this function will be moved to a separate file as logic grows
+const getBabySize = (days: Number): String => {
+  if (days > 0) {
+    return String(days)
+  }
+  return '100 microns in diameter'
+}
+
+interface babyProps {
+  days: number
+  comparisonType: string
+}
+interface babyInputProps extends babyProps {
+  daysSetter: (event: ChangeEvent<HTMLInputElement>) => void
+  comparisonTypeSetter: (event: ChangeEvent<HTMLSelectElement>) => void
+}
+
+const BabyOutputs: FC<babyProps> = ({ days }) => {
   return (
     <div>
-      Hard coding 0 days literal response.
-      100 microns in diameter
+      Hard coding {days} days literal response.
+      {getBabySize(days)}
     </div>
   )
 }
 
-const BabyInputs: FC = () => {
+const BabyInputs: FC<babyInputProps> = ({ days, daysSetter, comparisonType, comparisonTypeSetter }) => {
   return (
     <form >
-      <label htmlFor="comparisonType" > Choose a comparison type:</label>
-      <select id="comparisonType" name="comparisonType">
-        <option value="Literal">Literal</option>
+      <label htmlFor="comparisonType" > Choose a comparison type: </label>
+      <select id="comparisonType" name="comparisonType" value={comparisonType} onChange={comparisonTypeSetter}>
+        <option value='Literal' >Literal</option>
       </select>
       <br />
       <label htmlFor="days">Days since conception:</label>
-      <input type='number' id='days' ></input>
+      <input type='number' id='days' value={days} onChange={daysSetter}></input>
     </form>
   )
 }
 
 const Baby: FC = () => {
+  const [days, setDays] = useState(0)
+  const [comparisonType, setComparisonType] = useState('Literal')
+
+  const handleDaysChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const daysVal: number = +event.currentTarget.value
+    setDays(daysVal)
+  }
+
+  const handleComparisonTypeChange = (event: ChangeEvent<HTMLSelectElement>): void => {
+    setComparisonType(event.currentTarget.value)
+  }
+
   return (
     <div className="App">
       <div className="App-header">
@@ -51,7 +71,7 @@ const Baby: FC = () => {
       <div className="main-block">
         <div className='content'>
           <div className='image'>
-            <BabyOutputs />
+            <BabyOutputs days={days} comparisonType={comparisonType} />
           </div>
         </div>
         <div className='inputs'>
@@ -59,7 +79,12 @@ const Baby: FC = () => {
             Please adjust these inputs to specify the size of your baby and the app will attempt to find a suitable comparison.
           </div>
           <br />
-          <BabyInputs />
+          <BabyInputs
+            days={days}
+            daysSetter={handleDaysChange}
+            comparisonType={comparisonType}
+            comparisonTypeSetter={handleComparisonTypeChange}
+          />
         </div>
       </div>
 
