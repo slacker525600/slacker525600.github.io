efc8aaa73774191b012f109ead1af3ca0450b561
slacker525600 <slacker525600@yahoo.com>
2022-11-16 14:29:57 -0600

    Added a json blob to start comparisons, removed some hard coded values in favor of using that blob, added some comparison logic if no exact match found, bit of a messy commit with commented out blocks

--- slacker525600/src/Baby.tsx
+++ slacker525600/src/Baby.tsx
@@ -1,5 +1,7 @@
 import React, { ChangeEvent, FC, useState } from 'react'
+import { babyInputProps, babyProps } from './babyTypes'
 import FooterLinks from './footerLinks'
+import getBabySizeDiv, { getComparisonTypes } from './sizeLogic'
 
 /*
 Current idea is to provide an easy to use baby size comparison application.
@@ -8,76 +10,11 @@ then move to all the other things (types/weightvslength/)
 then worry about UX of interaction of above ... ratios lulz
 */
 
-const grams = [0.00075,
-  0.0015,
-  0.00307,
-  0.06125,
-  0.125,
-  0.25,
-  0.5,
-  1,
-  2,
-  4,
-  7,
-  14,
-  23,
-  43,
-  70,
-  100,
-  140,
-  190,
-  240,
-  300,
-  360,
-  430,
-  500,
-  600,
-  660,
-  760,
-  875,
-  1000,
-  1150,
-  1320,
-  1500,
-  1700,
-  1920,
-  2150,
-  2380,
-  2620,
-  2860,
-  3080,
-  3290,
-  3460,
-  3600,
-  3690
-]
-
-// this function will be moved to a separate file as logic grows
-const getBabySize = (weeks: number, comparisonType: string): string => {
-  const message = `Your Baby is ${comparisonType}ly: `
-  let size: string = ''
-  if (weeks > 0) {
-    size = String(grams[weeks]) + ' grams '
-  } else {
-    size = '100 microns in diameter'
-  }
-  return message + size
-}
-
-interface babyProps {
-  weeks: number
-  comparisonType: string
-}
-interface babyInputProps extends babyProps {
-  weeksSetter: (event: ChangeEvent<HTMLInputElement>) => void
-  comparisonTypeSetter: (event: ChangeEvent<HTMLSelectElement>) => void
-}
-
 const BabyOutputs: FC<babyProps> = ({ weeks, comparisonType }) => {
   return (
     <div>
       At {weeks} weeks... <br />
-      {getBabySize(weeks, comparisonType)}
+      {getBabySizeDiv({ weeks, comparisonType })}
     </div>
   )
 }
@@ -87,7 +24,11 @@ const BabyInputs: FC<babyInputProps> = ({ weeks, weeksSetter, comparisonType, co
     <form >
       <label htmlFor="comparisonType" > Choose a comparison type: </label>
       <select id="comparisonType" name="comparisonType" value={comparisonType} onChange={comparisonTypeSetter}>
-        <option value='Literal' >Literal</option>
+        {
+          getComparisonTypes().map((comparisonType) => {
+            return <option value={comparisonType} key={comparisonType} >{comparisonType}</option>
+          })
+        }
       </select>
       <br />
       <label htmlFor="weeks">Weeks since conception:</label>
